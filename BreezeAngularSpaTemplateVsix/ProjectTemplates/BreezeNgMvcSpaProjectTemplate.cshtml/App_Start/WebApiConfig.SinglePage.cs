5605dda83dc24c541f24bec314c382bce2e1eb46
Ward Bell <wardbell@hotmail.com>
2013-04-23 00:57:47 -0700

    In WebApiConfig undo change to route prefix from 'api' to 'breeze' The Web API default shouldn't have changed.

--- BreezeAngularSpaTemplateVsix/ProjectTemplates/BreezeNgMvcSpaProjectTemplate.cshtml/App_Start/WebApiConfig.SinglePage.cs
+++ BreezeAngularSpaTemplateVsix/ProjectTemplates/BreezeNgMvcSpaProjectTemplate.cshtml/App_Start/WebApiConfig.SinglePage.cs
@@ -12,7 +12,7 @@ namespace $safeprojectname$
         {
             //config.Routes.MapHttpRoute(
             //    name: "DefaultApi",
-            //    routeTemplate: "breeze/{controller}/{id}",
+            //    routeTemplate: "api/{controller}/{id}",
             //    defaults: new { id = RouteParameter.Optional }
             //);
 
